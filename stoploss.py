9a448f206a970c0a2a0e507b15882f942205c584
cronos2810 <hgpaz.2810@gmail.com>
2021-03-16 21:21:01 -0300

    Add decimals adjustment option

--- stoploss.py
+++ stoploss.py
@@ -1,14 +1,30 @@
 # Simple Stop Loss Calculator
 # HGP 05-Mar-2021
 
+###########
+# Imports #
+###########
+
 import os
 
+#############
+# Variables #
+#############
+
+# Decimals
+dc = 4
+
 # Leverage xN
-lv = 10
+lv = 20
+
 # Stop Loss Percentage
-sl = 5
+sl = 20
 # Take Profit Percentage
-tp = 10
+tp = 40
+
+########
+# Main #
+########
 
 while True:
     os.system('clear')
@@ -16,9 +32,10 @@ while True:
     print ('\n - Input 0 to exit.')
     print (' - Input 1 for Long Position')
     print (' - Input 2 for Short Position')
-    print (' - Input 3 for Leverage Adjustment (Default x1)')
-    print (' - Input 4 for Take Profit Adjustment (Default +3%)')
-    print (' - Input 5 for Stop Loss Adjustment (Default -1%)')
+    print (' - Input 3 for Leverage Adjustment (Default x20)')
+    print (' - Input 4 for Take Profit Adjustment (Default +40%)')
+    print (' - Input 5 for Stop Loss Adjustment (Default -20%)')
+    print (' - Input 6 for Decimals Adjustment (Default 4, for example: 1.0239)')
 
     print ('\n [info] Current values:\n')
 
@@ -48,35 +65,35 @@ while True:
         # lv = 10
         # How much is 10% of 5000?
         # result = 500
-        lv_percentage = round(float((entry * lv) / 100), 2)
+        lv_percentage = round(float((entry * lv) / 100), dc)
 
         # How much is 1% of "entry"
         # Example: 
         # entry = 5000 
         # How much is 1% of 5000?
         # result = 50
-        one_percentage = round(float((entry * 1) / 100), 2)
+        one_percentage = round(float((entry * 1) / 100), dc)
 
         # "lv_percentage" is what percent of 1%?
         # Example: 
         # 500 is what percent of 50?
         # result = 10%
-        lv_one_percentage = round(float((one_percentage * 100) / lv_percentage), 2)
+        lv_one_percentage = round(float((one_percentage * 100) / lv_percentage), dc)
 
         # How much % is "lv_one_percentage" of 1%?
         # Example: 
         # How much is 10% of 50?
         # result = 5
         # Multiplied by tp & sl
-        target_price_tp = round(float((lv_one_percentage * one_percentage) / 100), 2) * tp
-        target_price_sl = round(float((lv_one_percentage * one_percentage) / 100), 2) * sl
+        target_price_tp = round(float((lv_one_percentage * one_percentage) / 100), dc) * tp
+        target_price_sl = round(float((lv_one_percentage * one_percentage) / 100), dc) * sl
 
         # "entry" + "target_price" (tp & sl)
-        target_price_sum_tp = round(float(entry + target_price_tp), 2)
-        target_price_sum_sl = round(float(entry - target_price_sl), 2)
+        target_price_sum_tp = round(float(entry + target_price_tp), dc)
+        target_price_sum_sl = round(float(entry - target_price_sl), dc)
 
         # Risk/Reward Ratio Calculation
-        rr = round(float( round(float((entry * tp) / 100), 2) / round(float((entry * sl) / 100), 2) ), 2)
+        rr = round(float( round(float((entry * tp) / 100), dc) / round(float((entry * sl) / 100), dc) ), 2)
         
         print ('\n [info] Current values:\n')
 
@@ -106,35 +123,35 @@ while True:
         # lv = 10
         # How much is 10% of 5000?
         # result = 500
-        lv_percentage = round(float((entry * lv) / 100), 2)
+        lv_percentage = round(float((entry * lv) / 100), dc)
 
         # How much is 1% of "entry"
         # Example: 
         # entry = 5000 
         # How much is 1% of 5000?
         # result = 50
-        one_percentage = round(float((entry * 1) / 100), 2)
+        one_percentage = round(float((entry * 1) / 100), dc)
 
         # "lv_percentage" is what percent of 1%?
         # Example: 
         # 500 is what percent of 50?
         # result = 10%
-        lv_one_percentage = round(float((one_percentage * 100) / lv_percentage), 2)
+        lv_one_percentage = round(float((one_percentage * 100) / lv_percentage), dc)
 
         # How much % is "lv_one_percentage" of 1%?
         # Example: 
         # How much is 10% of 50?
         # result = 5
         # Multiplied by tp & sl
-        target_price_tp = round(float((lv_one_percentage * one_percentage) / 100), 2) * tp
-        target_price_sl = round(float((lv_one_percentage * one_percentage) / 100), 2) * sl
+        target_price_tp = round(float((lv_one_percentage * one_percentage) / 100), dc) * tp
+        target_price_sl = round(float((lv_one_percentage * one_percentage) / 100), dc) * sl
 
         # "entry" + "target_price" (tp & sl)
-        target_price_sum_tp = round(float(entry - target_price_tp), 2)
-        target_price_sum_sl = round(float(entry + target_price_sl), 2)
+        target_price_sum_tp = round(float(entry - target_price_tp), dc)
+        target_price_sum_sl = round(float(entry + target_price_sl), dc)
 
         # Risk/Reward Ratio Calculation
-        rr = round(float( round(float((entry * tp) / 100), 2) / round(float((entry * sl) / 100), 2) ), 2)
+        rr = round(float( round(float((entry * tp) / 100), dc) / round(float((entry * sl) / 100), dc) ), 2)
         
         print ('\n [info] Current values:\n')
 
@@ -173,6 +190,14 @@ while True:
         print (' [*] Insert new value (Example: 3): ')
 
         sl = float(input('\n >>> '))
+
+    # Decimals
+    elif option == '6':
+        os.system('clear')
+        print ('\n    *** Decimals adjustment ***\n')
+        print (' [*] Insert new value (Example: 3): ')
+
+        dc = float(input('\n >>> '))
 
     # Wrong Option
     else:
